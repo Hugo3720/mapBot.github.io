3ab99b4f7d392d0cfa3a112adfb9532ebfc94067
Hugo3720 <40397245+Hugo3720@users.noreply.github.com>
2024-05-29 22:31:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let tg = window.Telegram.Webapp;
 
+const hash = window.location.hash.slice(1);
+console.log(hash);
+
 const params = new URLSearchParams(hash);
 console.log(params.get('tgWebAppStartParam'));
 
